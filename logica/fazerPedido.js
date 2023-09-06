4fbf0d2986bc251d7c26f73c717a736544128424
Tácio Soares <taciofsoares@gmail.com>
2023-09-06 16:21:22 -0300

    51

--- logica/fazerPedido.js
+++ logica/fazerPedido.js
@@ -4,5 +4,5 @@ botaoZap.addEventListener('click', chamaNoZap)
 
 function chamaNoZap() {
     
-    window.open(`https://wa.me//5571992819692?text=Olá,%20olhei%20o%20catálogo%20AZUNI%20e%20tenho%20interesse%20nisso:${produtoComprado}%20o%20total%20da%20minha%20compra%20foi%20de%20${total.toFixed(2)}.`)
+    window.open(`https://wa.me//557186686018?text=Olá,%20olhei%20o%20catálogo%20AZUNI%20e%20tenho%20interesse%20nisso:${produtoComprado}%20o%20total%20da%20minha%20compra%20foi%20de%20${total.toFixed(2)}.`)
 }
