6045b600791247ece04f9ce4e8c2679a7b191c19
Tácio Soares <taciofsoares@gmail.com>
2023-09-07 18:07:12 -0300

    ds

--- logica/fazerPedido.js
+++ logica/fazerPedido.js
@@ -7,7 +7,7 @@ function chamaNoZap() {
 
     var cep = document.querySelector('#cep').value
     if(cep) {
-        window.open(`https://wa.me//557186686018?text=Olá,%20olhei%20o%20catálogo%20AZUNI%20e%20tenho%20interesse%20em:%0A%0A${produtoComprado}%0ATotal:%0AR$%20${total.toFixed(2)}.%0A%0AGostaria%20de%20saber%20o%20valor%20do%20frete.%0AO%20meu%20CEP%20é:%0A${cep}.`)
+        window.open(`https://wa.me//557186686018?text=Olá,%20olhei%20o%20catálogo%20AZUNI%20e%20tenho%20interesse%20em:%0A%0A${produtoComprado}%0ASubtotal:%0AR$%20${total.toFixed(2)}.%0A%0AGostaria%20de%20saber%20o%20valor%20do%20frete.%0AO%20meu%20CEP%20é:%0A${cep}.`)
     } else {
         window.alert('Por favor, digite seu CEP no campo para que possamos calcular o seu frete.')
     }
@@ -22,7 +22,7 @@ function verificaRepetidos(compra, index) {
             quantidade += 1
         }
     })
-    if(!(produtoComprado.includes(`${quantidade} ${compra.nome}.%0A`))) {
-        produtoComprado += `${quantidade} ${compra.nome}.%0A`
+    if(!(produtoComprado.includes(`${quantidade}x ${compra.nome}.%0A`))) {
+        produtoComprado += `${quantidade}x ${compra.nome}.%0A`
     }
 }
