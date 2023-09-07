507bbf36505a1c88676cb3a27ff1a37670589b81
Tácio Soares <taciofsoares@gmail.com>
2023-09-07 16:52:31 -0300

    Ok

--- logica/fazerPedido.js
+++ logica/fazerPedido.js
@@ -5,7 +5,14 @@ function chamaNoZap() {
     
     carrinho.forEach(verificaRepetidos)
 
-    window.open(`https://wa.me//557186686018?text=Olá,%20olhei%20o%20catálogo%20AZUNI%20e%20tenho%20interesse%20nisso:%0A%0A${produtoComprado}%0A%0AO%20total%20da%20minha%20compra%20foi%20de:%0AR$%20${total.toFixed(2)}.`)
+    var cep = document.querySelector('#cep').value
+    if(cep) {
+        window.open(`https://wa.me//557186686018?text=Olá,%20olhei%20o%20catálogo%20AZUNI%20e%20tenho%20interesse%20nisso:%0A%0A${produtoComprado}%0A%0AO%20total%20da%20minha%20compra%20foi%20de:%0AR$%20${total.toFixed(2)}.%0A%0AO%20meu%20CEP%20é:%0A%0A${cep}.`)
+    } else {
+        window.alert('Por favor, digite seu CEP no campo para que possamos calcular o seu frete.')
+    }
+
+    
 }
 
 function verificaRepetidos(compra, index) {
