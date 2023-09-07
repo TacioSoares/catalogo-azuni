0105ea2adbfd53cb59a67efd75e6b72478c2d7e0
Tácio Soares <taciofsoares@gmail.com>
2023-09-07 14:10:31 -0300

    sdf

--- logica/fazerPedido.js
+++ logica/fazerPedido.js
@@ -5,8 +5,8 @@ botaoZap.addEventListener('click', chamaNoZap)
 function chamaNoZap() {
     
     carrinho.forEach(verificaRepetidos)
-    
-    window.open(`https://wa.me//557186686018?text=Olá,%20olhei%20o%20catálogo%20AZUNI%20e%20tenho%20interesse%20nisso:${produtoComprado}%20o%20total%20da%20minha%20compra%20foi%20de%20${total.toFixed(2)}.`)
+
+    window.open(`https://wa.me//557186686018?text=Olá,%20olhei%20o%20catálogo%20AZUNI%20e%20tenho%20interesse%20nisso:${produtoComprado}%20o%20total%20da%20minha%20compra%20foi%20de%20R$%20${total.toFixed(2)}.`)
 }
 
 function verificaRepetidos(compra, index) {
