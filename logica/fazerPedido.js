d27ba6d010589d7622d129bb0d4093e54bfbd4b4
Tácio Soares <taciofsoares@gmail.com>
2023-09-07 14:31:03 -0300

    ok

--- logica/fazerPedido.js
+++ logica/fazerPedido.js
@@ -1,12 +1,12 @@
 const botaoZap = document.querySelector('#botao-comprar')
-
+const quebraDeLinha = '\n'
 botaoZap.addEventListener('click', chamaNoZap)
 
 function chamaNoZap() {
     
     carrinho.forEach(verificaRepetidos)
 
-    window.open(`https://wa.me//557186686018?text=Olá,%20olhei%20o%20catálogo%20AZUNI%20e%20tenho%20interesse%20nisso:${produtoComprado}%20o%20total%20da%20minha%20compra%20foi%20de%20R$%20&text=${total.toFixed(2)}.`)
+    window.open(`https://wa.me//557186686018?text=Olá,%20olhei%20o%20catálogo%20AZUNI%20e%20tenho%20interesse%20nisso:${quebraDeLinha}${quebraDeLinha}${produtoComprado}${quebraDeLinha}${quebraDeLinha}%20o%20total%20da%20minha%20compra%20foi%20de%20R$%20${quebraDeLinha}${total.toFixed(2)}.`)
 }
 
 function verificaRepetidos(compra, index) {
