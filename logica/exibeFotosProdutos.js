ec186102f5cab9513a05fabbca31180b976a72f8
Tácio Soares <taciofsoares@gmail.com>
2023-09-08 17:55:27 -0300

    teste

--- logica/exibeFotosProdutos.js
+++ logica/exibeFotosProdutos.js
@@ -2,9 +2,12 @@
 const pecasNoCatalogo = Array.from(document.querySelectorAll('.container-produto'))
 var posicao = 0
 
+let arrastando = false;
+let posicaoInicialX 
+let posicaoElementoX
+
 window.addEventListener('mousedown', function(e){
     posicao = e.pageY
-    console.log(posicao)
 })
 
 
@@ -44,16 +47,57 @@ function mostraTodasAsFotos(imagens) {
     
     // ADICIONA A FUNÇÃO FECHAR NO SPAN
     spanFecha.addEventListener('click', fechaFotos)
+
     // INSERE A SECTION EM DETERMINADA POSICAO DENTRO DO BODY
     sectionFotosGrandes.style.top = `${posicao-400}px`
     document.body.appendChild(sectionFotosGrandes)
     
     // DESABILITA O SCROLL DA PÁGINA
     document.body.style.overflowY = 'hidden'
+
+    // CRIA O ARRASTO COM DEDO/MOUSE
+    arrastoDaDiv(divContainer)
 }
 
+function arrastoDaDiv(divContainer) {
+
+    divContainer.addEventListener("mousedown", iniciarArrasto);
+    divContainer.addEventListener("touchstart", iniciarArrasto);
+
+}
+
+function iniciarArrasto(event) {
+    arrastando = true;
+    posicaoInicialX = event.clientX || event.touches[0].clientX;
+    posicaoElementoX = event.target.getBoundingClientRect().left;
+  
+    // Adicione eventos de escuta para continuar o arrasto e parar o arrasto.
+    document.addEventListener("mousemove", arrastar);
+    document.addEventListener("touchmove", arrastar);
+    document.addEventListener("mouseup", pararArrasto);
+    document.addEventListener("touchend", pararArrasto);
+}
+  
+function arrastar(event) {
+    if (!arrastando) return;
+    const posX = event.clientX || event.touches[0].clientX;
+  
+    const deltaX = posX - posicaoInicialX;
+  
+    event.target.style.left = posicaoElementoX + deltaX + "px";
+}
+  
+function pararArrasto() {
+    console.log('soltou')
+    arrastando = false;
+    document.removeEventListener("mousemove", arrastar);
+    document.removeEventListener("touchmove", arrastar);
+    document.removeEventListener("mouseup", pararArrasto);
+    document.removeEventListener("touchend", pararArrasto);
+}
+  
+
 function fechaFotos(event) {
-    console.log(event.target.parentNode)
     // REMOVE A SECÇÃO DO HTML
     document.body.removeChild(event.target.parentNode)
     document.body.style.overflowY = ''
